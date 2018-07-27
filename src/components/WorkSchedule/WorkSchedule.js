9d1d469f2a6a8684ef713fdd45be13dc31d666f0
Arthur Tkachenko <atherdon@gmail.com>
2018-07-27 23:17:27 +0300

    +

---
+++ src/components/WorkSchedule/WorkSchedule.js
@@ -0,0 +1,46 @@
+import React, {
+  Component 
+} from 'react';
+import { Container, Row } from 'reactstrap';
+import { ScheduleRender } from './Schedule';
+import { TimeLine } from './Timeline';
+
+import './WorkSched.css';
+
+class WorkSched extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            mode: false
+        };
+        this.changeMode = this.changeMode.bind(this);
+    }
+    
+    changeMode() {
+        this.setState({
+            mode: !this.state.mode
+        })
+    }
+    render() {
+      return(
+            <div>
+                <div className='header'>
+                  <h1>
+                    Schedule
+                  </h1>
+          <button onClick={this.changeMode}>Switch Mode</button>
+                </div>
+                <Container>
+                    <Row>
+                        <div className='timeline'>
+                          <TimeLine mode={this.state.mode}/>
+                        </div>
+                        <ScheduleRender mode={this.state.mode}/>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export { WorkSchedule }
---
+++ src/components/WorkSchedule/WorkSchedule.test.js
@@ -0,0 +1,1 @@
+WorkSchedule.test.js
