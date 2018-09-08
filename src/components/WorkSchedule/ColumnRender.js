6e08af3e95ab8a3d57cb1cdba87f1a73af8bd293
Vadim <vadim.putrov@gmail.com>
2018-09-08 21:29:10 +0300

    connect fetch

--- src/components/WorkSchedule/ColumnRender.js
+++ src/components/WorkSchedule/ColumnRender.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import { data }   from '../../data/Data_WorkSchedule';
-// import data from '@groceristar/groceristar-fetch/mealCalendar'
+// import { data }   from '../../data/Data_WorkSchedule';
+import data from '@groceristar/groceristar-fetch/mealCalendar'
 import { rendererFunc } from './Methods.js';
 
 class ColumnRender extends Component {
 
     Schedule() {
 
-      var result = data[0]["recipes"][this.props.index];
+      var result =
+          data.getDishByWeek("week1")[0]["recipes"][this.props.index+1];
               return rendererFunc(result, this.props.mode)
 
     }
