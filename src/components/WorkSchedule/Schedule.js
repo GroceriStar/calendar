ef1d5d370572b8033e0a289e86cd90fac702ed34
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-10-03 12:38:44 +0300

    cleanup

--- src/components/WorkSchedule/Schedule.js
+++ src/components/WorkSchedule/Schedule.js
@@ -1,34 +1,25 @@
-import React, { Component } from 'react';
-import { ListGroup, 
-    ListGroupItem 
-} from 'reactstrap';
-import { weekdayName } from '../../data/days';
-import { ColumnRender } from './ColumnRender';
-import { defaultBlockHeight } from '../../data/style_vars';
-import { Col } from 'reactstrap';
-
+import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import { weekdayName } from "../../data/days";
+import { ColumnRender } from "./ColumnRender";
+import { defaultBlockHeight } from "../../data/style_vars";
+import { Col } from "reactstrap";
 
 class ScheduleRender extends Component {
-    render() {
-        return(
-               
-            weekdayName.map((day, index) =>
-                    <Col sm="2">
-                <div key={index} className='data-class'>
-                    <ListGroup>
-                        <ListGroupItem style={defaultBlockHeight}>
-                            {day}
-                        </ListGroupItem>
-                        <ColumnRender mode={this.props.mode} 
-                                key={index} 
-                                index={index} />
-                        </ListGroup>
-                </div>
-                            </Col>
-                )
-              
-            );
-        }
+  render() {
+    // weekdayName.map((day, index) => console.log(index) );
+
+    return weekdayName.map((day, index) => (
+      <Col sm="2">
+        <div key={index} className="data-class">
+          <ListGroup>
+            <ListGroupItem style={defaultBlockHeight}>{day}</ListGroupItem>
+            <ColumnRender mode={this.props.mode} key={index} index={index} />
+          </ListGroup>
+        </div>
+      </Col>
+    ));
+  }
 }
 
-export { ScheduleRender }
+export { ScheduleRender };
