47e5c0598d486e8e5b50260a04279be8ff155d87
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-10-03 06:54:04 +0300

    sandbox updates

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Meal from './components/Calendar/Meal';
 import { WorkSchedule } from './components/WorkSchedule/WorkSchedule';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 class Router extends Component {
   render(){
     return (
