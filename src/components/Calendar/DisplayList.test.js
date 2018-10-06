a855901ec64010a4f1f6deba29c35f83237703af
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-10-06 14:55:05 +0300

    works

--- src/components/Calendar/DisplayList.test.js
+++ src/components/Calendar/DisplayList.test.js
@@ -1,19 +1,20 @@
 import React from "react";
-import { shallow, configure } from "enzyme";
+import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { DisplayList } from "./DisplayList";
+import DisplayList from "./DisplayList";
 
 import data from "@groceristar/groceristar-fetch/chickenKyiv";
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
 describe("<DisplayList /> component shallow only methods ", () => {
 
-  const recipe = data.getRecipe()[0];
+    test("renders without crashing", () => {
+    const recipe = data.getRecipe()[0];
 
-  it("renders without crashing", () => {
-    const wrapper = shallow(<DisplayList />);
-    // const wrapper = shallow(<DisplayList data={recipe.ingredients} />);
+    // const wrapper = shallow(<DisplayList />);
+    const wrapper = shallow(<DisplayList data={recipe.ingredients} />);
+
     expect(wrapper.contains(<ul></ul>)).toBe(true);
     // expect;
   });
