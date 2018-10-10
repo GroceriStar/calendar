63bdcfbe62a328cc89d8f858252fd812b8519b34
Vadim <vadim.putrov@gmail.com>
2018-10-10 20:39:53 +0300

    add random week

--- src/components/WorkSchedule/Schedule.js
+++ src/components/WorkSchedule/Schedule.js
@@ -7,7 +7,10 @@ import shortid from "shortid"
 
 
 class ScheduleRender extends Component {
+
+
   render() {
+    let randomWeek = "week" + Math.floor(Math.random() * 11);
     // weekdayName.map((day, index) => console.log(index) );
 
     return weekdayName.map((day, index) => (
@@ -15,7 +18,7 @@ class ScheduleRender extends Component {
         <div key={index} className="data-class">
           <ListGroup>
             <ListGroupItem style={defaultBlockHeight}>{day}</ListGroupItem>
-            <ColumnRender mode={this.props.mode} key={shortid.generate()} index={index} />
+            <ColumnRender mode={this.props.mode} key={shortid.generate()} index={index}  randomWeek={randomWeek}/>
           </ListGroup>
         </div>
       </Col>
